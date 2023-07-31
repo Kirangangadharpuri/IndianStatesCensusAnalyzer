b7492cf38a219250b753f302d98824ce1a080932
Kiran <kiranpuri532@gmail.com>
2023-07-31 09:05:47 +0530

    [Kiran]Add\: Read data of state census file

---
+++ IndianStatesCensusAnalyzer_Problem/IndianStatesCensusAnalyzer_Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyzer_Problem
+{
+    public class Program
+    {
+        public static string path = @"C:\Users\HP\Desktop\RFP-288\CesusAnalyzer\IndianStatesCensusAnalyzer\IndianStatesCensusAnalyzer_Problem\IndianStatesCensusAnalyzer_Problem\Files\Files\StateCensusData.csv";
+        static void Main(string[] args)
+        {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+            stateCensusAnalyser.ReadStateCensusData(path);
+        }
+    }
+}
---
+++ IndianStatesCensusAnalyzer_Problem/IndianStatesCensusAnalyzer_Test/StatesCensus_Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndianStatesCensusAnalyzer_Test;
+
+namespace IndianStatesCensusAnalyzer_Test
+{
+    public class StatesCensus_Test
+    {
+    }
+}
