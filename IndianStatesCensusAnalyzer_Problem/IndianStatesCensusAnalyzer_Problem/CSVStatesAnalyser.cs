6ec8db453b0f460fe0eed2f31f2618cd2af6ddaf
Kiran <kiranpuri532@gmail.com>
2023-08-01 19:02:08 +0530

    [Kiran]Add:\ Read data from StatecodeData csv file

---
+++ IndianStatesCensusAnalyzer_Problem/IndianStatesCensusAnalyzer_Problem/CSVStatesAnalyser.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyzer_Problem
+{
+    public class CSVStatesAnalyser
+    {
+        public int ReadStatesCode(string path_two)
+        {
+            using (var reader = new StreamReader(path_two))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InstalledUICulture))
+                {
+                    var record = csv.GetRecords<StateCodeData>().ToList();
+                    foreach (var data in record)
+                    {
+                        Console.WriteLine(data.SrNo + " " + data.StateName + " " + data.TIN + " " + data.StateCode);
+                    }
+                    return record.Count() - 1;
+
+                }
+            }
+        }
+    }
+}
