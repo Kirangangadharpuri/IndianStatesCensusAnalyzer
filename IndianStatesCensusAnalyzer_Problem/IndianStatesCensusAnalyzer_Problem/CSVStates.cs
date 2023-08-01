e493ba928c19aad2889c1fe28000ee5b2e5731de
Kiran <kiranpuri532@gmail.com>
2023-08-01 18:49:08 +0530

    [Kiran]Add:\ Read data from StatecodeData csv file

---
+++ IndianStatesCensusAnalyzer_Problem/IndianStatesCensusAnalyzer_Problem/CSVStates.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyzer_Problem
+{
+    public class CSVStates
+    {
+        public void ReadStatesCode(string path_two)
+        {
+            using (var reader = new StreamReader(path_two))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InstalledUICulture))
+                {
+                    var record = csv.GetRecords<StateCodeData>().ToList();
+                    foreach (var data in record)
+                    {
+                        Console.WriteLine(data.SrNo + " " + data.StateName + " " + data.TIN + " " + data.StateCode);
+                    }
+                }
+            }
+        }
+    }
+}
--- IndianStatesCensusAnalyzer_Problem/IndianStatesCensusAnalyzer_Problem/Program.cs
+++ IndianStatesCensusAnalyzer_Problem/IndianStatesCensusAnalyzer_Problem/Program.cs
@@ -9,10 +9,13 @@ namespace IndianStatesCensusAnalyzer_Problem
     public class Program
     {
         public static string path = @"C:\Users\HP\Desktop\RFP-288\CesusAnalyzer\IndianStatesCensusAnalyzer\IndianStatesCensusAnalyzer_Problem\IndianStatesCensusAnalyzer_Problem\Files\Files\StateCensusData.csv";
+        public static string path_two = @"C:\Users\HP\Desktop\RFP-288\CesusAnalyzer\IndianStatesCensusAnalyzer\IndianStatesCensusAnalyzer_Problem\IndianStatesCensusAnalyzer_Problem\Files\Files\StateCodeData.csv";
         static void Main(string[] args)
         {
-            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
-            stateCensusAnalyser.ReadStateCensusData(path);
+            //StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+            //stateCensusAnalyser.ReadStateCensusData(path);
+            CSVStates cSVStates = new CSVStates();
+            cSVStates.ReadStatesCode(path_two);
         }
     }
 }
